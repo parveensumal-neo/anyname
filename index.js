f221ab163ba1c74c011709a9622be7487bb007b3
parveensumal-neo <parvee.s@neoito.com>
2022-11-09 16:59:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const fnfn1 = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello fnfn1`}))
+  res.end()
+  
+}
+
+export default fnfn1
